93da43c2e2e0ec37bf7241976fc9a989429f0cc6
ZivDero <kirill.andriiashin@gmail.com>
2023-12-27 18:19:50 +0300

    Allowed connected overlays to connect to other connected overlays

--- src/TSMapEditor/Models/ConnectedOverlayType.cs
+++ src/TSMapEditor/Models/ConnectedOverlayType.cs
@@ -1,3 +1,4 @@
+using System;
 using Rampastring.Tools;
 using System.Collections;
 using System.Collections.Generic;
@@ -32,8 +33,6 @@ namespace TSMapEditor.Models
 
             ConnectionMask = new BitArray(connectionMaskString.Select(c => c == '1').ToArray());
 
-            Frames = new List<ConnectedOverlayFrame>();
-
             for (int i = 0; i < FrameCount; i++)
             {
                 string overlayName = iniSection.GetStringValue($"Frame{i}.Overlay", null);
@@ -59,10 +58,26 @@ namespace TSMapEditor.Models
             }
         }
 
-        public string Name { get; set; }
-        public int FrameCount { get; set; }
-        public BitArray ConnectionMask { get; set; }
-        public List<ConnectedOverlayFrame> Frames { get; set; }
+        public string Name { get; protected set; }
+        public int FrameCount { get; protected set; }
+        public BitArray ConnectionMask { get; protected set; }
+        public List<ConnectedOverlayFrame> Frames { get; protected set; } = new();
+        public List<ConnectedOverlayType> RelatedOverlays { get; protected set; } = new();
+
+        public void InitializeRelatedOverlays(IniSection iniSection, List<ConnectedOverlayType> connectedOverlays)
+        {
+            var relatedOverlayNames = iniSection.GetStringValue("RelatedOverlays", string.Empty)
+                .Split(',', StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var relatedOverlayName in relatedOverlayNames)
+            {
+                var relatedOverlay = connectedOverlays.Find(co => co.Name == relatedOverlayName);
+                if (relatedOverlay == null)
+                    throw new INIConfigException($"Connected overlay type {Name} has an invalid related overlay {relatedOverlayName}!");
+
+                RelatedOverlays.Add(relatedOverlay);
+            }
+        }
 
         public ConnectedOverlayFrame GetOverlayForCell(IMutationTarget mutationTarget, Point2D cellCoords)
         {
@@ -83,26 +98,48 @@ namespace TSMapEditor.Models
             return Frames.Find(frame => ((BitArray)frame.ConnectsTo.Clone()).Xor(connectionMask).OfType<bool>().All(e => !e));
         }
 
-        public bool ContainsOverlay(Overlay overlay)
+        public bool ContainsOverlay(Overlay overlay, bool searchRelated = true)
         {
             if (overlay == null)
                 return false;
 
-            return ContainsOverlay(overlay.OverlayType, overlay.FrameIndex);
+            return ContainsOverlay(overlay.OverlayType, overlay.FrameIndex, searchRelated);
         }
 
-        public bool ContainsOverlay(OverlayType overlayType, int frameIndex)
+        public bool ContainsOverlay(OverlayType overlayType, int frameIndex, bool searchRelated = true)
+        {
+            return GetContainedConnectedOverlayType(overlayType, frameIndex, searchRelated) != null;
+        }
+
+        public ConnectedOverlayType GetContainedConnectedOverlayType(Overlay overlay, bool searchRelated = true)
+        {
+            if (overlay == null)
+                return null;
+
+            return GetContainedConnectedOverlayType(overlay.OverlayType, overlay.FrameIndex, searchRelated);
+        }
+
+        public ConnectedOverlayType GetContainedConnectedOverlayType(OverlayType overlayType, int frameIndex, bool searchRelated = true)
         {
             foreach (var frame in Frames)
             {
                 if (overlayType == frame.OverlayType &&
                     frameIndex == frame.FrameIndex)
                 {
-                    return true;
+                    return this;
                 }
             }
 
-            return false;
+            if (searchRelated)
+            {
+                foreach (var relatedOverlay in RelatedOverlays)
+                {
+                    if (relatedOverlay.ContainsOverlay(overlayType, frameIndex, searchRelated = false))
+                        return relatedOverlay;
+                }
+            }
+
+            return null;
         }
     }
 
