702c28dbeefbc39fff186817304b7b730fb8d07f
ZivDero <kirill.andriiashin@gmail.com>
2024-01-13 01:11:47 +0300

    Add dynamic rendered voxel image size

--- src/TSMapEditor/Rendering/ObjectRenderers/VxlRenderer.cs
+++ src/TSMapEditor/Rendering/ObjectRenderers/VxlRenderer.cs
@@ -1,16 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
-using CNCMaps.FileFormats;
-using Microsoft.Xna.Framework;
-using Color = Microsoft.Xna.Framework.Color;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Rampastring.XNAUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using TSMapEditor.CCEngine;
-using Vector2 = Microsoft.Xna.Framework.Vector2;
-using Vector3 = Microsoft.Xna.Framework.Vector3;
 using static TSMapEditor.CCEngine.VxlFile;
+using Color = Microsoft.Xna.Framework.Color;
+using Vector3 = Microsoft.Xna.Framework.Vector3;
 
 namespace TSMapEditor.Rendering.ObjectRenderers
 {
@@ -23,29 +20,12 @@ namespace TSMapEditor.Rendering.ObjectRenderers
 
         private const float NearClip = 0.01f; // the near clipping plane distance
         private const float FarClip = 100f; // the far clipping plane distance
-        private const int ImageWidth = 100;
-        private const int ImageHeight = 100;
-        private static readonly Matrix Projection = Matrix.CreateOrthographic(ImageWidth * ModelScale, ImageHeight * ModelScale, NearClip, FarClip);
 
         public static Texture2D Render(GraphicsDevice graphicsDevice, byte facing, RampType ramp, VxlFile vxl, HvaFile hva, Palette palette, VplFile vpl = null, bool forRemap = false)
         {
-            var renderTarget = new RenderTarget2D(graphicsDevice, ImageWidth, ImageHeight, false, SurfaceFormat.Color, DepthFormat.Depth24);
-            Renderer.PushRenderTarget(renderTarget);
+            /*********** Voxel space setup **********/
             
-            graphicsDevice.Clear(Color.Transparent);
-            graphicsDevice.DepthStencilState = DepthStencilState.Default;
-
             float rotationFromFacing = 2 * (float)Math.PI * ((float)facing / Constants.FacingMax);
-
-            // Rotates to the game's north
-            Matrix rotateToWorld = Matrix.CreateRotationZ(-(MathHelper.ToRadians(135) + rotationFromFacing));
-            rotateToWorld *= Matrix.CreateRotationX(MathHelper.ToRadians(-120));
-            Matrix scale = Matrix.CreateScale(ModelScale, ModelScale, ModelScale);
-
-            BasicEffect basicEffect = new BasicEffect(graphicsDevice);
-            basicEffect.VertexColorEnabled = true;
-            basicEffect.View = View;
-            basicEffect.Projection = Projection;
 
             Matrix tilt = Matrix.Identity;
 
@@ -58,6 +38,11 @@ namespace TSMapEditor.Rendering.ObjectRenderers
                 Vector3 tiltAxis = Vector3.Transform(Vector3.UnitX, rotateTiltAxis);
                 tilt = Matrix.CreateFromAxisAngle(tiltAxis, MathHelper.ToRadians(30));
             }
+
+            // Rotates to the game's north
+            Matrix rotateToWorld = Matrix.CreateRotationZ(-(MathHelper.ToRadians(135) + rotationFromFacing));
+            rotateToWorld *= Matrix.CreateRotationX(MathHelper.ToRadians(-120));
+            Matrix scale = Matrix.CreateScale(ModelScale, ModelScale, ModelScale);
 
             var vertexColorIndexedData = new List<VertexData>();
 
@@ -93,6 +78,24 @@ namespace TSMapEditor.Rendering.ObjectRenderers
                 vertexColorIndexedData.AddRange(sectionVertexData);
             }
 
+            /********** Rendering *********/
+
+            Matrix world = rotateToWorld * tilt * scale;
+            Rectangle imageBounds = GetBounds(vxl, hva, world);
+            Matrix projection = Matrix.CreateOrthographic(imageBounds.Width, imageBounds.Height, NearClip, FarClip);
+
+            var renderTarget = new RenderTarget2D(graphicsDevice, Convert.ToInt32(imageBounds.Width / ModelScale), Convert.ToInt32(imageBounds.Height / ModelScale), false, SurfaceFormat.Color, DepthFormat.Depth24);
+            Renderer.PushRenderTarget(renderTarget);
+
+            graphicsDevice.Clear(Color.Transparent);
+            graphicsDevice.DepthStencilState = DepthStencilState.Default;
+
+            BasicEffect basicEffect = new BasicEffect(graphicsDevice);
+            basicEffect.VertexColorEnabled = true;
+            basicEffect.View = View;
+            basicEffect.Projection = projection;
+            basicEffect.World = world;
+
             VertexBuffer vertexBuffer = new VertexBuffer(graphicsDevice,
                 typeof(VertexPositionColorNormal), vertexColorIndexedData.Count, BufferUsage.None);
 
@@ -112,9 +115,6 @@ namespace TSMapEditor.Rendering.ObjectRenderers
 
             graphicsDevice.Indices = triangleListIndexBuffer;
             graphicsDevice.SetVertexBuffer(vertexBuffer);
-
-            Matrix sectionWorld = rotateToWorld * tilt * scale;
-            basicEffect.World = sectionWorld;
 
             foreach (EffectPass pass in basicEffect.CurrentTechnique.Passes)
             {
@@ -142,6 +142,34 @@ namespace TSMapEditor.Rendering.ObjectRenderers
             0, 90, 180, -90
         };
 
+        private class VertexData
+        {
+            public VertexData(Vector3 position, Palette palette, byte colorIndex, Vector3 normal)
+            {
+                Position = position;
+                Palette = palette;
+                ColorIndex = colorIndex;
+                Normal = normal;
+            }
+
+            public VertexPositionColorNormal ToVertexPositionColorNormal(bool remapOnly = false)
+            {
+                if (remapOnly)
+                {
+                    if (ColorIndex is < 0x10 or > 0x1F)
+                    {
+                        return new VertexPositionColorNormal(Position, Color.Magenta, Normal);
+                    }
+                }
+
+                return new VertexPositionColorNormal(Position, Palette.Data[ColorIndex].ToXnaColor(), Normal);
+            }
+
+            public Vector3 Position;
+            public Palette Palette;
+            public byte ColorIndex;
+            public Vector3 Normal;
+        }
 
         private static List<VertexData> RenderVoxel(VxlFile.Voxel voxel, Vector3 normal, Palette palette, Vector3 scale)
         {
@@ -213,33 +241,63 @@ namespace TSMapEditor.Rendering.ObjectRenderers
             }
         }
 
-        private class VertexData
+        private static Rectangle GetBounds(VxlFile vxl, HvaFile hva, Matrix transform)
         {
-            public VertexData(Vector3 position, Palette palette, byte colorIndex, Vector3 normal)
+            Rectangle ret = Rectangle.Empty;
+            foreach (var section in vxl.Sections)
             {
-                Position = position;
-                Palette = palette;
-                ColorIndex = colorIndex;
-                Normal = normal;
+                var sectionRotation = hva.LoadMatrix(section.Index);
+                sectionRotation.M41 *= section.HvaMultiplier * section.ScaleX;
+                sectionRotation.M42 *= section.HvaMultiplier * section.ScaleY;
+                sectionRotation.M43 *= section.HvaMultiplier * section.ScaleZ;
+
+                var sectionTranslation = Matrix.CreateTranslation(section.MinBounds);
+                var sectionTransform = sectionTranslation * sectionRotation;
+
+                transform = sectionTransform * transform;
+
+                // floor rect of the bounding box
+                Vector3 floorTopLeft = new Vector3(0, 0, 0);
+                Vector3 floorTopRight = new Vector3(section.SpanX, 0, 0);
+                Vector3 floorBottomRight = new Vector3(section.SpanX, section.SpanY, 0);
+                Vector3 floorBottomLeft = new Vector3(0, section.SpanY, 0);
+
+                // ceil rect of the bounding box
+                Vector3 ceilTopLeft = new Vector3(0, 0, section.SpanZ);
+                Vector3 ceilTopRight = new Vector3(section.SpanX, 0, section.SpanZ);
+                Vector3 ceilBottomRight = new Vector3(section.SpanX, section.SpanY, section.SpanZ);
+                Vector3 ceilBottomLeft = new Vector3(0, section.SpanY, section.SpanZ);
+
+                // apply transformations
+                floorTopLeft = Vector3.Transform(floorTopLeft, transform);
+                floorTopRight = Vector3.Transform(floorTopRight, transform);
+                floorBottomRight = Vector3.Transform(floorBottomRight, transform);
+                floorBottomLeft = Vector3.Transform(floorBottomLeft, transform);
+
+                ceilTopLeft = Vector3.Transform(ceilTopLeft, transform);
+                ceilTopRight = Vector3.Transform(ceilTopRight, transform);
+                ceilBottomRight = Vector3.Transform(ceilBottomRight, transform);
+                ceilBottomLeft = Vector3.Transform(ceilBottomLeft, transform);
+
+                int FminX = (int)Math.Floor(Math.Min(Math.Min(Math.Min(floorTopLeft.X, floorTopRight.X), floorBottomRight.X), floorBottomLeft.X));
+                int FmaxX = (int)Math.Ceiling(Math.Max(Math.Max(Math.Max(floorTopLeft.X, floorTopRight.X), floorBottomRight.X), floorBottomLeft.X));
+                int FminY = (int)Math.Floor(Math.Min(Math.Min(Math.Min(floorTopLeft.Y, floorTopRight.Y), floorBottomRight.Y), floorBottomLeft.Y));
+                int FmaxY = (int)Math.Ceiling(Math.Max(Math.Max(Math.Max(floorTopLeft.Y, floorTopRight.Y), floorBottomRight.Y), floorBottomLeft.Y));
+
+                int TminX = (int)Math.Floor(Math.Min(Math.Min(Math.Min(ceilTopLeft.X, ceilTopRight.X), ceilBottomRight.X), ceilBottomLeft.X));
+                int TmaxX = (int)Math.Ceiling(Math.Max(Math.Max(Math.Max(ceilTopLeft.X, ceilTopRight.X), ceilBottomRight.X), ceilBottomLeft.X));
+                int TminY = (int)Math.Floor(Math.Min(Math.Min(Math.Min(ceilTopLeft.Y, ceilTopRight.Y), ceilBottomRight.Y), ceilBottomLeft.Y));
+                int TmaxY = (int)Math.Ceiling(Math.Max(Math.Max(Math.Max(ceilTopLeft.Y, ceilTopRight.Y), ceilBottomRight.Y), ceilBottomLeft.Y));
+
+                int minX = Math.Min(FminX, TminX);
+                int maxX = Math.Max(FmaxX, TmaxX);
+                int minY = Math.Min(FminY, TminY);
+                int maxY = Math.Max(FmaxY, TmaxY);
+
+                ret = Rectangle.Union(ret, new Rectangle(minX, minY, maxX - minX, maxY - minY));
             }
 
-            public VertexPositionColorNormal ToVertexPositionColorNormal(bool remapOnly = false)
-            {
-                if (remapOnly)
-                {
-                    if (ColorIndex is < 0x10 or > 0x1F)
-                    {
-                        return new VertexPositionColorNormal(Position, Color.Magenta, Normal);
-                    }
-                }
-
-                return new VertexPositionColorNormal(Position, Palette.Data[ColorIndex].ToXnaColor(), Normal);
-            }
-
-            public Vector3 Position;
-            public Palette Palette;
-            public byte ColorIndex;
-            public Vector3 Normal;
+            return ret;
         }
     }
 }
