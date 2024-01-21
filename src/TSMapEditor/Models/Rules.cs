bbaec3dd0ea386fcf3bbc781d5a3ed8b51333dc2
ZivDero <kirill.andriiashin@gmail.com>
2024-01-21 04:07:01 +0300

    Skip adding duplicate types

--- src/TSMapEditor/Models/Rules.cs
+++ src/TSMapEditor/Models/Rules.cs
@@ -313,6 +313,11 @@ namespace TSMapEditor.Models
 
                 var objectType = typeof(T);
 
+                // If we encounter the same object listed twice, don't create a duplicate
+                var iniNameProperty = objectType.GetProperty("ININame");
+                if (targetList.Find(t => iniNameProperty?.GetValue(t)?.ToString() == typeName) != null)
+                    continue;
+
                 // We assume that the type has a constructor
                 // that takes a single string (ININame) as a parameter
                 var constructor = objectType.GetConstructor(new Type[] { typeof(string) });
