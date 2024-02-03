fdcb3e5e1cffa7a3795572e7325d824a9a399627
ZivDero <kirill.andriiashin@gmail.com>
2024-02-03 15:53:27 +0300

    Make common draw params a ref struct

--- src/TSMapEditor/Rendering/ObjectRenderers/CommonDrawParams.cs
+++ src/TSMapEditor/Rendering/ObjectRenderers/CommonDrawParams.cs
@@ -1,11 +1,11 @@
 ﻿namespace TSMapEditor.Rendering.ObjectRenderers
 {
-    public struct CommonDrawParams
+    public readonly ref struct CommonDrawParams
     {
-        public string IniName;
-        public ShapeImage MainImage;
-        public VoxelModel MainModel;
-        public VoxelModel TurretModel;
-        public VoxelModel BarrelModel;
+        public string IniName { get; init; }
+        public ShapeImage MainImage { get; init; }
+        public VoxelModel MainModel { get; init; }
+        public VoxelModel TurretModel { get; init; }
+        public VoxelModel BarrelModel { get; init; }
     }
 }
